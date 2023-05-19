902d529fc442f1a3715ac961703609a83ad43421
Abhishek7Tech <abhishektab45@gmail.com>
2023-05-19 12:31:24 +0530

    added useReducer

--- src/contexts/SliderContext.js
+++ src/contexts/SliderContext.js
@@ -1,5 +1,6 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer, useState } from "react";
 import questions from "../questions";
+
 export const SliderContext = createContext({
   questions: [],
   question: 0,
@@ -8,20 +9,60 @@ export const SliderContext = createContext({
   setSlider: () => {},
 });
 
+const initialState = {
+  slider: false,
+  question: 0,
+};
+
+function sliderReducer(state, action) {
+  const { type, payload } = action;
+  switch (type) {
+    case "SET_SLIDER": {
+      return {
+        slider: true,
+        question: state.question,
+      };
+    }
+
+    case "SET_QUESTION_NEXT": {
+      return {
+        slider: true,
+        question: state.question + 1,
+      };
+    }
+
+    case "SET_QUESTION_PREVIOUS": {
+      return {
+        slider: true,
+        question: state.question - 1,
+      };
+    }
+    default:
+      throw Error("UNKNOWN ACTION", action.type);
+  }
+}
+
 export const SliderProvider = ({ children }) => {
-  const [slider, setSlider] = useState(false);
-  const [question, setQuestion] = useState(0);
+  // const [slider, setSlider] = useState(false);
+  // const [question, setQuestion] = useState(0);
+
+  const [{ slider, question }, dispatch] = useReducer(
+    sliderReducer,
+    initialState
+  );
 
   const sliderHandler = (e) => {
     e.preventDefault();
-    setSlider(!slider);
+    dispatch({ type: "SET_SLIDER" });
+    // setSlider(!slider);
   };
   function nextBtnHandler(e) {
     console.log("Clicked");
     e.preventDefault();
-    if (question !== 2) {
+    if (question !== 2 ) {
       // setSlider(!slider);
-      setQuestion(question + 1);
+      dispatch({ type: "SET_QUESTION_NEXT" });
+      // setQuestion(question + 1);
     }
   }
 
@@ -29,12 +70,20 @@ export const SliderProvider = ({ children }) => {
     e.preventDefault();
 
     if (question !== 0) {
-      setQuestion(question - 1);
+      dispatch({ type: "SET_QUESTION_PREVIOUS" });
+      // setQuestion(question - 1);
     }
   }
   let questionsArray = questions;
 
-  const value = { slider, sliderHandler, questionsArray, question,nextBtnHandler, previousButtonHandler};
+  const value = {
+    slider,
+    sliderHandler,
+    questionsArray,
+    question,
+    nextBtnHandler,
+    previousButtonHandler,
+  };
   return (
     <SliderContext.Provider value={value}>{children}</SliderContext.Provider>
   );
